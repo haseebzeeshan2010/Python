918384939a25c30efe5790a9bbfb1e5f340d3b88
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2024-10-12 22:28:49 +0100

    Use findall function

--- Regex/example_search.py
+++ Regex/example_search.py
@@ -1,11 +1,15 @@
 import re
 
-text = "Sample text"
-pattern = "Sample"
+text = "Sample text, 199.99"
+pattern = r"\d"
 
 match = re.search(pattern, text)
+
+findall = re.findall(pattern,text)
 
 if match:
     print("match found")
 else:
     print("no match found")
+
+print(findall)
