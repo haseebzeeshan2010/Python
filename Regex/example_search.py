391efb98d817e9da0f28b2cb49c996beeb121128
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2024-10-12 22:47:24 +0100

    Use compile Regex function with example

--- Regex/example_search.py
+++ Regex/example_search.py
@@ -9,6 +9,17 @@ findall = re.findall(pattern,text) # returns all instances as an array
 
 subn = re.subn(r'\b\w{4}\b', '****', text) # returns substituted string and number of replaced parts
 
+split = re.split(pattern, text) # returns split string at every occurance of the pattern
+
+split3 = re.split(pattern, text, 3) # returns split string at every occurance of the pattern but only 3 times
+
+
+#Compiling a regex function so it is more efficient if used repeatedly
+compileregex = re.compile(pattern)
+
+compilefind = compileregex.findall(text)
+
+
 if match:
     print("match found")
 else:
@@ -16,3 +27,6 @@ else:
 
 print(findall)
 print(subn)
+print(split)
+print(split3)
+print(compilefind)
