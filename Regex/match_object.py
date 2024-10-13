e36b47f8892edb092d83855f4ad229e006110172
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2024-10-13 20:47:53 +0100

    Add information about match object

---
+++ Regex/match_object.py
@@ -0,0 +1,16 @@
+#Match always has boolean value of true
+
+#Has methods to return information about match
+
+#string() returns original string that was provided to be matched
+
+#group() is a method that returns the string that was found
+
+#groups() is a method that returns the tuble to find all groups that were found
+
+#start() returns the starting position of the matched string
+
+#end() returns the ending position of the matched string
+
+#span() returns a tuple containing the starting and ending position of the matched strings within a tuple
+
