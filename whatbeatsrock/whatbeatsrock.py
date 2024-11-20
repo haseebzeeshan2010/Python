abe80dcf99d720ba28319b219de82ff5c92a6cac
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2024-11-20 16:48:42 +0000

    Create simple starter program for whatbeatsrock

---
+++ whatbeatsrock/whatbeatsrock.py
@@ -0,0 +1,9 @@
+
+alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+word = ["Letter ", "The Letter", "The alphabetic letter ", "The letters "]
+
+for i in word:
+    for j in alphabets:
+        print(i+j)
+        input("continue?")
+
